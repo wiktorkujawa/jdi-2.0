a3997f2a51d012b259ce7d30e0aec092bf60ee77
wiktorkujawa <wiktorkujawa1993@gmail.com>
2025-06-13 00:59:48 +0200

    feature/payload-custom-fields - enable general import of block components

---
+++ src/fields/components/index.ts
@@ -0,0 +1,7 @@
+export { default as WYSIWYGBlock } from './WYSIWYGBlock';
+export { default as QuoteBlock } from './QuoteBlock';
+export { default as HeadingBlock } from './HeadingBlock';
+export { default as HeadingCopyBlock } from './HeadingCopyBlock';
+export { default as SliderBlock } from './SliderBlock';
+
+
