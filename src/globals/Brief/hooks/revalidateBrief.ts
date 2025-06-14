740224941f76da74cb093fa9c20c3923246180f8
wiktorkujawa <wiktorkujawa1993@gmail.com>
2025-06-14 02:42:33 +0200

    feature/brief-global - created Brief global

---
+++ src/globals/Brief/hooks/revalidateBrief.ts
@@ -0,0 +1,6 @@
+import { revalidateTag } from "next/cache"
+import { GlobalAfterChangeHook } from "payload"
+
+export const revalidateBrief: GlobalAfterChangeHook = async () => {
+    revalidateTag("brief");
+} 
--- src/globals/index.ts
+++ src/globals/index.ts
@@ -3,3 +3,4 @@ export { Footer } from "./Footer";
 export { ProjectList } from "./ProjectList";
 export { Experience } from "./Experience";
 export { Education } from "./Education";
+export { Brief } from "./Brief";
