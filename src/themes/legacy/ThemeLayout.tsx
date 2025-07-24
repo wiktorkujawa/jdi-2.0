f28110b046fd93a65f2f032ca3e219522928485b
wiktorkujawa <wiktorkujawa1993@gmail.com>
2025-07-24 03:51:09 +0200

    feature/legacy-font - set legacy font and fixed css typos

--- src/themes/legacy/ThemeLayout.tsx
+++ src/themes/legacy/ThemeLayout.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 
+import { Figtree } from 'next/font/google'
+
 import Footer from './components/organisms/Footer'
 import Header from './components/organisms/Header'
 import './css/style.css'
 
+const figtree = Figtree({
+  subsets: ['latin'],
+})
+
 export default function ThemeLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={figtree.className}>
       <body className="min-h-screen flex flex-col" data-theme="legacy">
         <Header />
         <main className="flex-1">{children}</main>
